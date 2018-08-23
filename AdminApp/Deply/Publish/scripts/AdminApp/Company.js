fa913e5044e437012816ba6317edf3fad5e24fe2
karthik <Madhavan_Karthik@cat.com>
2018-08-23 22:02:35 +0530

    changes

---
+++ AdminApp/Deply/Publish/scripts/AdminApp/Company.js
@@ -0,0 +1,120 @@
+﻿$(document).ready(function () {
+    removeError();
+});
+function removeError() {
+    $('#CompanyName').removeClass("is-invalid");
+    $('#PrimaryPhoneNo').removeClass("is-invalid");
+    $('#Address').removeClass("is-invalid");
+    $('#City').removeClass("is-invalid");
+    $('#Country').removeClass("is-invalid");
+    $('#PrimaryMailID').removeClass("is-invalid");
+    $('#GSTNO').removeClass("is-invalid");
+    $('#State').removeClass("is-invalid");
+    $('#Pincode').removeClass("is-invalid");
+}
+function btnSave() {
+    removeError();
+    var valRes = true;
+    if ($('#CompanyName').val() == '') {
+        $('#CompanyName').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#PrimaryPhoneNo').val() == '') {
+        $('#PrimaryPhoneNo').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#Address').val() == '') {
+        $('#Address').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#City').val() == '') {
+        $('#City').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#Country').val() == '') {
+        $('#Country').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#GSTNO').val() == '') {
+        $('#GSTNO').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#PrimaryMailID').val() == '') {
+        $('#PrimaryMailID').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#State').val() == '') {
+        $('#State').addClass("is-invalid");
+        valRes = false;
+    }
+    if ($('#Pincode').val() == '') {
+        $('#Pincode').addClass("is-invalid");
+        valRes = false;
+    }
+    return valRes;
+}
+function loadCompanyDetails(compID)
+{
+    //Ajax Call to Save and Update
+    $.ajax({
+        type: "GET",
+        data: { "CompanyID": compID},
+        url: "/CompanySettings/GetCompanyDetails",
+        contentType: 'application/json',
+        dataType: "json",
+        beforeSend: function () {
+
+        },
+        success: function (data) {
+            if (data == "error") {
+                $("#divModal").hide();
+            }
+            else {
+                $("#cmpName").html(data.CompanyName);
+                $("#cmpMail").html(data.PrimaryMailID);
+                $("#cmpPhone").html(data.PrimaryPhoneNo);
+                $("#cmpFax").html(data.Fax);
+                $("#cmpWebsite").html(data.Website);
+                $("#cmpGST").html(data.GSTNO);
+                $("#cmpAddr").html(data.Address);
+                $("#cmpAddr1").html(data.Address1);
+                $("#cmpCity").html(data.City);
+                $("#cmpState").html(data.State);
+                $("#cmpCountry").html(data.Country);
+                $("#cmpPincode").html(data.Pincode);
+
+            }
+        },
+        failure: function (data) {
+            $("#divModal").hide();
+        },
+        error: function (data) {
+            $("#divModal").hide();
+        },
+        complete: function () {
+
+        }
+    });
+}
+
+function editRoleDetails(compID) {
+    var date = new Date(); // Javascript date object
+    var link = '@Url.Action("Test", "CompanySettings")';  // url should be enclosed by single quotes.
+    var args = {
+        param1: date.toISOString(),  // make sure that the date is in Javascript date object and converted to ISO string for proper casting in c#
+        param2: date.toISOString(),
+        param3: 'somevalue'
+    };
+
+    $.ajax({
+        type: "GET",
+        url: link, // url of your action
+        data: args, // parameters if available
+        dataType: "json",
+        success: function (data) {
+            window.location.href = data.redirectUrl;
+        },
+        error: function (httpRequest, textStatus, errorThrown) {  // detailed error messsage
+        }
+    });
+}
---
+++ AdminApp/Deply/Publish/scripts/AdminApp/Role.js
@@ -0,0 +1,121 @@
+﻿function addNewRole() {
+    $("#divTitle").html("New Role");
+    $("#btnSave").text("Save");
+    $('#txtRollName').val('');
+    $("#hdnRoleID").val(null);
+    showhide();
+    ClearErrorMsg();
+}
+function cancel() {
+    $("#newRole").hide();
+    ClearErrorMsg();
+}
+function saveRole() {
+    //Input Validation 
+    var valResult;
+    var inputValue = $('#txtRollName').val();
+    inputValue = inputValue.replace(/-/g, '');
+    if (inputValue != '') {
+        var regex = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+        var isSplChar = regex.test(inputValue);
+        if(isSplChar)
+        {
+            $('#lblRollName').html("** Should not contain special characters.").show();
+            $('#txtRollName').addClass("is-invalid");
+            return false;
+        }
+    }
+    if (inputValue == '') {
+        $('#lblRollName').html("** Role Name is required.").show();
+        $('#txtRollName').addClass("is-invalid");
+        return false;
+    }
+
+    //Get Role ID Value
+    var hdnRID;
+    if ($("#hdnRoleID").val() != null && $("#hdnRoleID").val() != undefined)
+        hdnRID = parseInt($("#hdnRoleID").val());
+    else
+        hdnRID = parseInt(0);
+
+    //Populating Model Values 
+    var TModel = {
+        Name: $('#txtRollName').val(),
+        RoleId: hdnRID
+    };
+
+    //Ajax Call to Save and Update
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(TModel),
+        url: "/MasterSettings/SaveRole",
+        contentType: 'application/json',
+        dataType: "json",
+        beforeSend: function () {
+
+        },
+        success: function (data) {
+            if ( data == "error") {
+                return ShowErrorMsg(errorMsg.roleSubmitError);
+            }
+            $("#newRole").hide();
+            location.reload(true);
+        },
+        failure: function (data) {
+            return ShowErrorMsg(errorMsg.roleSubmitError);
+        },
+        error: function (data) {
+            return ShowErrorMsg(errorMsg.roleSubmitError);
+        },
+        complete: function () {
+
+        }
+    });
+}
+
+function editRoleDetails(roleID) {
+    //Get Role Name based on row click
+    $('#tblRole tr').each(function () {
+        if (!this.rowIndex) return; // skip first row
+        var rowRoleID = this.cells[0].innerHTML;
+        if (parseInt(roleID) == parseInt(rowRoleID)) {
+            var names = this.cells[1].innerHTML;
+            $('#txtRollName').val(names.replace(/ /g, ''));
+        }
+    });
+    $("#hdnRoleID").val(roleID);
+    $("#divTitle").html("Update Role");
+    $("#btnSave").text("Update");
+    showhide();
+    ClearErrorMsg();
+}
+function showhide() {
+    $("#newRole").show();
+    $('#txtRollName').removeClass("is-invalid");
+    $('#lblRollName').html("").hide();
+
+}
+$('#txtRollName').keyup(function () {
+    var inputValue = $('#txtRollName').val();
+    if (parseInt(inputValue) > 0) {
+        $('#lblRollName').html("** Should not contains numbers.").show();
+        $('#txtRollName').addClass("is-invalid");
+        return false;
+    }
+    inputValue = inputValue.replace(/-/g, '');
+    if (inputValue != '') {
+        var regex = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+        var isSplChar = regex.test(inputValue);
+        if (isSplChar) {
+            $('#lblRollName').html("** Should not contain special characters.").show();
+            $('#txtRollName').addClass("is-invalid");
+            return false;
+        }
+        else
+        {
+            $('#txtRollName').removeClass("is-invalid");
+            $('#lblRollName').html("").hide();
+        }
+    }
+    
+});
